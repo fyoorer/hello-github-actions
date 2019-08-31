8a114ad5beaac7983be9d50c81a0da043c03d37e
Aditya <adityagujar@gmail.com>
2019-08-31 19:08:53 -0400

    host

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -3,3 +3,14 @@
 sh -c "echo Hello world my name is $MY_NAME"
 which docker
 docker ps
+echo "\n\nIn the container"
+whoami
+ps aux
+cat /etc/issue
+echo "\n\nOn the host"
+docker run --privileged --name dddd -d debian
+docker ps
+docker exec dddd "echo Hello from host"
+docker exec dddd "whoami"
+docker exec dddd "cat /etc/passwd"
+
